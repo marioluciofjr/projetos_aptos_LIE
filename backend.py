a6fd6f138f5c6f77627eb28f2b07c8c20860b1ee
Mário Lúcio <marioluciofjr@gmail.com>
2024-08-21 23:52:58 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -279,14 +279,10 @@ if st.button("Limpar consulta"):
     st.session_state['resposta_faq'] = ""
     st.markdown("""
     <style>
-    .stButton > baseButton-secondary {
+    .stButton > button.st-emotion-cache-1vt4y43 ef3psqc13 {
         color: #f2f2f2;
         background-color: #230023;
         animation: fadein 2s;
-    }
-    @keyframes fadein {
-        from { opacity: 0; }
-        to { opacity: 1; }
     }
     </style>
     """, unsafe_allow_html=True)
